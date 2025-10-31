8cb69d83ae28c9de5015096392ac9046a7809932
Pedro Renato <prgiassi09@gmail.com>
2025-10-31 11:47:48 -0300

    Add files via upload

---
+++ aula2/Main.java
@@ -0,0 +1,32 @@
+// Aula prática "projeto251", quinta-feira, 30/10
+
+import java.util.Scanner;
+import java.sql.*;
+
+public class Main {
+
+    private Scanner leia;
+
+    public Main() {
+
+        leia= new Scanner(System.in);
+
+        System.out.println("\n== MENU PRINCIPAL ==\n\n" +
+                "1 - Inserir\n" +
+                "2 - Alterar");
+
+        int opcao = leia.nextInt();
+
+        if (opcao == 1) {
+            System.out.print("Digite o nome : ");
+            String nome = leia.nextLine();
+           // DriverManager.getConnection(URL, USUARIO, SENHA);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+}
